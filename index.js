19acd66403a2025fed94ac4c328ae702e3c3095a
Deo Raj <d.jrajsingh81@gmail.com>
2023-10-02 18:17:00 +0530

    Applying some validations

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
-const cors= require("cors");
+const cors = require("cors");
 require("./db/config");
-
 const User = require("./db/User");
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
-  resp.send(req.body);
+  result = result.toObject();
+  delete result.password;
+  resp.send(result);
 });
+
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User found" });
+    }
+  } else {
+    resp.send({ result: "No User found" });
+  }
+});
+
 app.listen(5000);
